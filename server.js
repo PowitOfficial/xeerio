0269a44c10073247887d6cb4aecc4be77ab9ae68
Jens Brehmen <jensbrehmen@MacBook-Pro-van-Jens.local>
2020-12-22 12:30:09 +0100

    Update package.json and rename server file

--- server.js
+++ server.js
@@ -305,7 +305,7 @@ app.prepare()
             return handle(req, res)
         })
 
-        server.listen(3000, (err) => {
+        server.listen(8080, (err) => {
             if (err) throw err
             console.log('> Ready on http://localhost:3000')
         })
